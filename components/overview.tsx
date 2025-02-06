f535629c36a6f8b5caf3bca859e8dbd6506ffee7
Mainstar <72015219+mainstar123@users.noreply.github.com>
2025-02-06 20:04:31 +0800

    Update home page description

--- components/overview.tsx
+++ components/overview.tsx
@@ -1,7 +1,5 @@
-import { motion } from 'framer-motion';
-import Link from 'next/link';
-
-import { MessageIcon, VercelIcon } from './icons';
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 export const Overview = () => {
   return (
@@ -14,37 +12,39 @@ export const Overview = () => {
       transition={{ delay: 0.5 }}
     >
       <div className="rounded-xl p-6 flex flex-col gap-8 leading-relaxed text-center max-w-xl">
-        <p className="flex flex-row justify-center gap-4 items-center">
-          <VercelIcon size={32} />
-          <span>+</span>
-          <MessageIcon size={32} />
-        </p>
         <p>
-          This is an{' '}
+          Chatbot powered by{" "}
           <Link
             className="font-medium underline underline-offset-4"
-            href="https://github.com/vercel/ai-chatbot"
+            href="https://nextjs.org/"
             target="_blank"
           >
-            open source
-          </Link>{' '}
-          chatbot template built with Next.js and the AI SDK by Vercel. It uses
-          the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">streamText</code>{' '}
-          function in the server and the{' '}
-          <code className="rounded-md bg-muted px-1 py-0.5">useChat</code> hook
-          on the client to create a seamless chat experience.
-        </p>
-        <p>
-          You can learn more about the AI SDK by visiting the{' '}
+            Next.js{" "}
+          </Link>
+          ,{" "}
           <Link
             className="font-medium underline underline-offset-4"
             href="https://sdk.vercel.ai/docs"
             target="_blank"
           >
-            docs
+            AI SDK{" "}
           </Link>
-          .
+          ,{" "}
+          <Link
+            className="font-medium underline underline-offset-4"
+            href="https://sdk.vercel.ai/docs"
+            target="_blank"
+          >
+            NextAuth.js
+          </Link>{" "}
+          by{" "}
+          <Link
+            className="font-medium underline underline-offset-4"
+            href="https://github.com/mainstar123"
+            target="_blank"
+          >
+            JCode
+          </Link>
         </p>
       </div>
     </motion.div>
